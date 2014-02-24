07f956324c759d8cdf8ff4b6309e6549c2fcf02c
yeegahngsong <yeegahngsong@koreanair.com>
2014-02-24 16:13:15 +0900

    modify application launching steps to enable running
    first runnable application status

--- src/government/service/validity/GovernmentProxy.java
+++ src/government/service/validity/GovernmentProxy.java
@@ -1,11 +1,13 @@
 package government.service.validity;
 
 import hill.hotel.executive.IBusinessRunnable;
+import hill.hotel.util.Log;
 
 import java.util.ArrayList;
 
 public final class GovernmentProxy {
 	
+	private static final int OFFSET = 10000;
 	private static GovernmentProxy mProxyEntity = new GovernmentProxy();
 	private ArrayList<IBusinessRunnable> mBusinessEnrollmentList;
 	
@@ -17,16 +19,43 @@ public final class GovernmentProxy {
 		return mProxyEntity;
 	}
 	
-	public boolean verifyLicenseValidity(int licenseNumber) {
-		IBusinessRunnable bizOwner = mBusinessEnrollmentList.get(licenseNumber);
-		return (bizOwner.getLicenseNumber() == licenseNumber);
+	public boolean verifyLicenseValidity(int licenseNumber, IBusinessRunnable bizOwner) {
+		if(bizOwner == null) {
+			Log.w("input bizOwner to verify is null");
+			return false;
+		}
+		if(bizOwner.getLicenseNumber() != licenseNumber) {
+			Log.w("input bizOwner has different licenseNumber to verify");
+			return false;
+		}
+		if(mBusinessEnrollmentList.isEmpty()) {
+			Log.w("No business owner registered yet");
+			return false;
+		}
+		IBusinessRunnable enrolledBizOwner = mBusinessEnrollmentList.get(extractLicenseNumber(licenseNumber));
+		if(bizOwner.equals(enrolledBizOwner)
+				&& (licenseNumber == enrolledBizOwner.getLicenseNumber())) {
+			Log.i("License Number has been verified: " + licenseNumber);
+			return true;
+		}
+		else {
+			return false;	
+		}
 	}
 
 	public boolean registerBusiness(IBusinessRunnable bizOwner) {
 		mBusinessEnrollmentList.add(bizOwner);
-		bizOwner.setLicenseNumber(mBusinessEnrollmentList.indexOf(bizOwner) + 10000); //10000 is offset
+		int licenseNumber = synthesizeLicenseNumber(mBusinessEnrollmentList.indexOf(bizOwner));
+		bizOwner.setLicenseNumber(licenseNumber);
 		return true;
-		
+	}
+	
+	/* mimic hash generation and reversing */
+	private int synthesizeLicenseNumber(int listIndex) {
+		return listIndex + OFFSET;
+	}
+	private int extractLicenseNumber(int licenseNumber) {
+		return licenseNumber - OFFSET;
 	}
 
 }
